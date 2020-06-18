6ab3e6e92688ad29843c32625daa289fa85cb383
parkdongsu <dongsu2005@naver.com>
2020-06-18 16:33:35 +0900

    Update app.R

--- inst/app.R
+++ inst/app.R
@@ -438,7 +438,7 @@ shinyApp(
     # Sending individual Json files into the activated Elasticsearch. This process can be performed separately  from the other processes before.
     observeEvent(input$send, {
       #if(exists(input$host|input$port)==T){
-      esConnection <- elastic::connect(host = '15.165.9.201',port = 9200, errors='complete') # port = input$port
+      #esConnection <- elastic::connect(host = '15.165.9.201',port = 9200, errors='complete') # port = input$port
       #} else{
       #  esConnection <- elastic::connect(errors='complete')
       #}
@@ -451,6 +451,8 @@ shinyApp(
       #print(jsonFolderPath)
       #unzip(zipfile = dataPath,exdir = jsonFolderPath)
       #file.remove(dataPath)
+      
+      c(1,2,3)
       
       #jsonToES(esConnection, indexName = input$indexName, jsonFolder = jsonFolderPath, dropIfExist = T)
     })
