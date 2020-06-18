3d0e2765d447721b22aecd81cf5db7482a6821e4
parkdongsu <dongsu2005@naver.com>
2020-06-18 15:45:34 +0900

    Update saveJson.R

--- R/saveJson.R
+++ R/saveJson.R
@@ -6,8 +6,8 @@
 #' @import tm
 #' @export
 
-saveJson <- function(JSON){
-  if(dir.exists(paste0(tempdir(), "/JSON"))==FALSE){dir.create(path = paste0(tempdir(), "/JSON"))}
+saveJson <- function(jsonPath,JSON){
+  if(dir.exists(paste0(jsonPath, "/JSON"))==FALSE){dir.create(path = paste0(jsonPath, "/JSON"))}
   for(i in 1:length(JSON)){
     JSON[i] <- tm::stripWhitespace(JSON[i])
     JSON[i] <- jsonlite::toJSON(JSON[i])
